a04f513be0c9d0b579542243a42e68fced5f2528
smil-thakur <smil.raj.thakur@gmail.com>
2023-02-09 17:27:10 +0530

    Initial Commit

---
+++ DailyHacks/DailyHacksApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyHacksApp.swift
+//  DailyHacks
+//
+//  Created by Smil on 09/02/23.
+//
+
+import SwiftUI
+
+@main
+struct DailyHacksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
